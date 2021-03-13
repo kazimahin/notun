6d986f7af8c1302030680a86b41a2451394c7e65
Kazi Maksud Ul Mahin <44231614+kazimahin@users.noreply.github.com>
2021-03-13 10:15:24 +0600

    Update App.js
    change in github

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-<<<<<<< HEAD
+ 
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,11 +18,10 @@ function App() {
         >
           Learn React a
         </a>
-      </header>
-=======
+  
     <h1>asdfasdf</h1>
-       </header>
->>>>>>> main
+ 
+ 
     </div>
   );
 }
